c0dd9d37d91f33bc7e09859f643c15ee2118bf43
sivapriyasangaraju <33112474+sivapriyasangaraju@users.noreply.github.com>
2017-10-26 11:12:01 +0530

    Add files via upload

---
+++ StringUtil.java
@@ -0,0 +1,11 @@
+package com.slokam.first.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringUtil {
+public void test()
+{
+	System.out.println("hello i am in Stringutil method....");
+}
+}
